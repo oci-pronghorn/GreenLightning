fc598b3e679537c3c20e1481c95d67498a484846
Nathan Tippy <nathan@tippy.name>
2017-07-06 09:10:05 -0500

    add needed constants

--- src/main/java/com/ociweb/gl/api/Behavior.java
+++ src/main/java/com/ociweb/gl/api/Behavior.java
@@ -2,4 +2,13 @@ package com.ociweb.gl.api;
 
 public interface Behavior {
 
+	public static final int ALL = GreenCommandChannel.DYNAMIC_MESSAGING | 
+            GreenCommandChannel.NET_REQUESTER | 
+            GreenCommandChannel.NET_RESPONDER;
+
+	public static final int DYNAMIC_MESSAGING = GreenCommandChannel.DYNAMIC_MESSAGING;
+	public static final int NET_REQUESTER = GreenCommandChannel.NET_REQUESTER;
+	public static final int NET_RESPONDER = GreenCommandChannel.NET_RESPONDER;
+
+	
 }
