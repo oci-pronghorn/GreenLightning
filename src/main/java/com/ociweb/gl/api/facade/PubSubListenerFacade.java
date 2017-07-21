f8d46a7eb0a29a094a4c06e8b309f1da8a4db860
ray-lo <raylo@go.wustl.edu>
2017-07-21 10:04:56 -0500

    moved interfaces around shuffle shuffle shufflin'

---
+++ src/main/java/com/ociweb/gl/api/facade/PubSubListenerFacade.java
@@ -0,0 +1,8 @@
+package com.ociweb.gl.api.facade;
+
+import com.ociweb.gl.api.ListenerFacade;
+import com.ociweb.gl.impl.PubSubListenerBase;
+
+public interface PubSubListenerFacade extends ListenerFacade, PubSubListenerBase{
+	
+}
---
+++ src/main/java/com/ociweb/gl/api/facade/StateChangeListenerFacade.java
@@ -0,0 +1,8 @@
+package com.ociweb.gl.api.facade;
+
+import com.ociweb.gl.api.ListenerFacade;
+import com.ociweb.gl.api.StateChangeListener;
+
+public interface StateChangeListenerFacade extends ListenerFacade, StateChangeListener{
+
+}
