d0b56e6e3cb08e3a6d3d6f2d5019d462cb5c72ef
Nathan Tippy <nathan@tippy.name>
2017-07-25 10:05:20 -0500

    added args

--- src/main/resources/archetype-resources/src/main/java/GreenLightning.java
+++ src/main/resources/archetype-resources/src/main/java/GreenLightning.java
@@ -5,7 +5,7 @@ import com.ociweb.gl.api.GreenRuntime;
 public class GreenLightning {
 
 	public static void main(String[] args) {
-		GreenRuntime.run(new ${artifactId}());
+		GreenRuntime.run(new ${artifactId}(),args);
 	}
 	
 }
