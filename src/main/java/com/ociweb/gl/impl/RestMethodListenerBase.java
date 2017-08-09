701e2e9b5b6ee18110ac02f2f23089f7236ca7fa
Nathan Tippy <nathan@tippy.name>
2017-08-08 20:00:06 -0500

    add missing file

---
+++ src/main/java/com/ociweb/gl/impl/RestMethodListenerBase.java
@@ -0,0 +1,22 @@
+package com.ociweb.gl.impl;
+
+import com.ociweb.gl.api.HTTPRequestReader;
+import com.ociweb.gl.api.RestMethodListener;
+import com.ociweb.pronghorn.network.ServerCoordinator;
+
+/**
+ * Functional interface for a handler of REST service events.
+ *
+ * @author Nathan Tippy
+ */
+@FunctionalInterface
+public interface RestMethodListenerBase extends RestMethodListener{
+
+	public static final int END_OF_RESPONSE = ServerCoordinator.END_RESPONSE_MASK;
+	public static final int CLOSE_CONNECTION = ServerCoordinator.CLOSE_CONNECTION_MASK;
+		
+
+    boolean restRequest(HTTPRequestReader request);
+    
+    
+}
