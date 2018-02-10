a4b8bbf59b6c1441ac96b48831aeeb8eded9ee2c
David Giovannini <giovanninid@objectcomputing.com>
2018-02-09 19:18:15 -0600

    Use names, not the type system.

--- src/main/java/com/ociweb/gl/impl/http/server/HTTPPayloadReader.java
+++ src/main/java/com/ociweb/gl/impl/http/server/HTTPPayloadReader.java
@@ -1,7 +1,7 @@
 package com.ociweb.gl.impl.http.server;
 import java.io.IOException;
 
-import com.ociweb.gl.api.PayloadablePredicate;
+import com.ociweb.gl.api.FailablePayloadReading;
 import com.ociweb.gl.impl.PayloadReader;
 import com.ociweb.pronghorn.network.config.*;
 import org.slf4j.Logger;
@@ -152,7 +152,7 @@ public class HTTPPayloadReader<S extends MessageSchema<S>> extends PayloadReader
 	}
 
 
-	public boolean openPayloadData(PayloadablePredicate reader) {
+	public boolean openPayloadDataFailable(FailablePayloadReading reader) {
 		if (hasRemainingBytes()) {
 			position(this, readFromEndLastInt(PAYLOAD_INDEX_LOCATION));
 			return reader.read(this);//even when we have zero length...
