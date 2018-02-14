a226d36e6366fae1d495885d66a358e4c2edd8e2
Nathan Tippy <nathantippy@gmail.com>
2018-02-14 17:13:12 -0600

    bring in these changes

--- src/main/java/com/ociweb/gl/impl/PrivateTopic.java
+++ src/main/java/com/ociweb/gl/impl/PrivateTopic.java
@@ -6,17 +6,22 @@ import com.ociweb.pronghorn.pipe.PipeConfig;
 
 public class PrivateTopic {
 
-	private Pipe<MessagePrivate> p;
+	private Pipe<MessagePrivate>[] p;
+	private int activeIndex=0;
+	
 	public final String topic;
 	
 	private final PipeConfig<MessagePrivate> config;
 	
-	public PrivateTopic(String topic, int messageCount, int messageSize, boolean hideLabels) {
+	public PrivateTopic(String topic, int messageCount, 
+			            int messageSize, boolean hideLabels,
+			            int parallelismTracks) {
 		this.topic = topic;
 		this.config = new PipeConfig<MessagePrivate>(MessagePrivate.instance, messageCount, messageSize);		
 		if (hideLabels) {
 			this.config.hideLabels(); //private topics can clutter if they show all the details.
 		}
+		this.p = (Pipe<MessagePrivate>[])new Pipe[parallelismTracks];
 	}
 	
 	public PrivateTopic(String topic, PipeConfig<MessagePrivate> config) {
@@ -25,10 +30,25 @@ public class PrivateTopic {
 	}
 
 	public Pipe<MessagePrivate> getPipe() {
-		if (null==p) {
-			p = PipeConfig.pipe(config);			
+		Pipe<MessagePrivate> result = p[activeIndex];
+		if (null == result) {
+			result = p[activeIndex] = PipeConfig.pipe(config);	
 		}
-		return p;
+		activeIndex = 0; //clear back to zero
+		return result;
+		
+		//one vs many is causing unconnected pipe
+		
+	}
+
+	public void cloneSource(int parallelInstanceUnderActiveConstruction) {
+		//store index so next getPipe() call picks it up
+		activeIndex = parallelInstanceUnderActiveConstruction;
+	}
+
+	public void cloneTarget(int parallelInstanceUnderActiveConstruction) {
+		//store index so next getPipe() call picks it up
+		activeIndex = parallelInstanceUnderActiveConstruction;
 	}
 	
 }
