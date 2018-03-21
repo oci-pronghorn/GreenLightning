038e49007fa5b37b93f0fd441bb1677d35a19f60
Nathan Tippy <nathantippy@gmail.com>
2018-03-21 13:52:55 -0500

    test update

--- src/test/java/com/ociweb/gl/example/parallel/NamedMessagePassingTest.java
+++ src/test/java/com/ociweb/gl/example/parallel/NamedMessagePassingTest.java
@@ -8,40 +8,43 @@ import com.ociweb.gl.test.ParallelClientLoadTesterConfig;
 import com.ociweb.gl.test.ParallelClientLoadTesterPayload;
 
 public class NamedMessagePassingTest {
+	///////////-XX:+UseLargePages 
+	//          -verbose:gc -Xloggc:gc.log -XX:+PrintGCTimeStamps -XX:+PrintGCDetails
+	
+	// -XX:MaxGCPauseMillis=5 -XX:+UseG1GC
+	// -XX:MaxDirectMemorySize=256m
 
 	@Test
 	public void runTest() {
 
-		//order supervisor blocks because it must have 1 incoming pipe for each
-		//of the N unit pipes coming in or data can be hidden !!!
-		
-		//need more threads for volume??
 		
 		boolean telemetry = true;
+		long rate = 2000;
 		
-		GreenRuntime.run(new NamedMessagePassingApp(telemetry));
+		GreenRuntime.run(new NamedMessagePassingApp(telemetry,rate));
 		
 		ParallelClientLoadTesterPayload payload 
 				= new ParallelClientLoadTesterPayload("{\"key1\":\"value\",\"key2\":123}");
 		
 		ParallelClientLoadTesterConfig config1 = 
 				new ParallelClientLoadTesterConfig(2, 50000, 8080, "/test", false);
+		config1.rate = rate;
+		config1.simultaneousRequestsPerTrackBits  = 4;
+		
 		GreenRuntime.testConcurrentUntilShutdownRequested(
 				new ParallelClientLoadTester(config1, null),
 				600_000);
 
 		ParallelClientLoadTesterConfig config2 = 
-				new ParallelClientLoadTesterConfig(1, 100_000, 8080, "/test", telemetry);
+				new ParallelClientLoadTesterConfig(2, 100_000, 8080, "/test", telemetry);
 		
-		config2.simultaneousRequestsPerTrackBits  = 1;
+		config2.simultaneousRequestsPerTrackBits  = 4;
 		config2.responseTimeoutNS = 0L;
-		
-		//config2.rate = 4_000L;
-		
+	
 		//For low latency
-		config2.rate = 1_000L; //TODO: may need to be bigger for slow windows boxes.
+		config2.rate = rate; //TODO: may need to be bigger for slow windows boxes.
 				
-		//TODO: ensure we have enought volume to make the optimization...
+		//TODO: ensure we have enough volume to make the optimization...
 		
 		GreenRuntime.testConcurrentUntilShutdownRequested(
 				new ParallelClientLoadTester(config2, null),
