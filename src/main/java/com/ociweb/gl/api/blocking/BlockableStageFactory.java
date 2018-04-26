c53d4dc391509286324d7c25a86e2b38ef847f42
Nathan Tippy <nathantippy@gmail.com>
2018-04-25 23:21:28 -0500

    factory for building blockable stage

--- src/main/java/com/ociweb/gl/api/blocking/BlockableStageFactory.java
+++ src/main/java/com/ociweb/gl/api/blocking/BlockableStageFactory.java
@@ -11,8 +11,8 @@ import com.ociweb.pronghorn.stage.scheduling.GraphManager;
 
 public class BlockableStageFactory {
 
-   public <T extends MessageSchema<T>, P extends MessageSchema<P>, Q extends MessageSchema<Q>> 
-       BlockingSupportStage<T, P, Q> buildStage(
+   public static <T extends MessageSchema<T>, P extends MessageSchema<P>, Q extends MessageSchema<Q>, B extends BlockingBehavior> 
+        BlockingSupportStage<T, P, Q> buildStage(
 	    		   GraphManager graphManager, 
 	    		   long timeoutNS, 
 	    		   int threadsCount,
@@ -20,8 +20,7 @@ public class BlockableStageFactory {
 	    		   Pipe<T> input,
 	    		   Pipe<P> output, 
 	    		   Pipe<Q> timeout, 
-	    		   Class<BlockingBehavior> clazz
-    		   ) {
+	    		   Class<B> clazz  ) {
 
 	   Choosable<T> chooser = new ChoosableLongField<T>(chooserLongFieldId, threadsCount, streamOffset(input));
 	   
@@ -50,27 +49,23 @@ public class BlockableStageFactory {
 	   } else {
 		   throw new UnsupportedOperationException("Not yet implemented");
 	   }
-	   
-	   
-	   
-	   
 	   return new BlockingSupportStage<T,P,Q>(graphManager, input, output, timeout, timeoutNS, chooser, blockables);
 	   
    }
 
-private <T extends MessageSchema<T>> int streamOffset(Pipe<T> input) {
-	////////////////////
-	   //lookup the position of the stream 
-	   int offsetToStream = -1;
-	   if (Pipe.isForSchema(input, MessagePrivate.class)) {
-		   offsetToStream = 0xFF & MessagePrivate.MSG_PUBLISH_1_FIELD_PAYLOAD_3;
-	   } else if (Pipe.isForSchema(input, MessageSubscription.class)) {
-		   offsetToStream = 0xFF & MessageSubscription.MSG_PUBLISH_103_FIELD_PAYLOAD_3;
-	   } else {
-		   throw new UnsupportedOperationException("Schema not supported: "+Pipe.schemaName(input));
-	   }
-	   ////////////////////
-	return offsetToStream;
-}
+	private static <T extends MessageSchema<T>> int streamOffset(Pipe<T> input) {
+		////////////////////
+		   //lookup the position of the stream 
+		   int offsetToStream = -1;
+		   if (Pipe.isForSchema(input, MessagePrivate.class)) {
+			   offsetToStream = 0xFF & MessagePrivate.MSG_PUBLISH_1_FIELD_PAYLOAD_3;
+		   } else if (Pipe.isForSchema(input, MessageSubscription.class)) {
+			   offsetToStream = 0xFF & MessageSubscription.MSG_PUBLISH_103_FIELD_PAYLOAD_3;
+		   } else {
+			   throw new UnsupportedOperationException("Schema not supported: "+Pipe.schemaName(input));
+		   }
+		   ////////////////////
+		return offsetToStream;
+	}
 	
 }
