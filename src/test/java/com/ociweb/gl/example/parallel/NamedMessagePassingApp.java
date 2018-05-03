f69629582136d69f3d893b6a81b8d463096104f3
Nathan Tippy <nathantippy@gmail.com>
2018-05-03 11:49:26 -0500

    merge

--- src/test/java/com/ociweb/gl/example/parallel/NamedMessagePassingApp.java
+++ src/test/java/com/ociweb/gl/example/parallel/NamedMessagePassingApp.java
@@ -1,14 +1,12 @@
 package com.ociweb.gl.example.parallel;
 
-import java.io.File;
-import java.io.IOException;
-
 import com.ociweb.gl.api.Builder;
 import com.ociweb.gl.api.GreenAppParallel;
 import com.ociweb.gl.api.GreenRuntime;
 import com.ociweb.json.JSONExtractor;
 import com.ociweb.json.JSONExtractorCompleted;
 import com.ociweb.json.JSONType;
+import com.ociweb.json.decode.JSONDecoder;
 import com.ociweb.pronghorn.network.config.HTTPHeaderDefaults;
 
 public class NamedMessagePassingApp implements GreenAppParallel {
@@ -34,7 +32,7 @@ public class NamedMessagePassingApp implements GreenAppParallel {
 	
 	@Override
 	public void declareConfiguration(Builder builder) {
-
+		
 		builder.useHTTP1xServer(8081)
 		       .useInsecureServer()
 		       //.logTraffic(false)
@@ -71,18 +69,20 @@ public class NamedMessagePassingApp implements GreenAppParallel {
 		builder.setGlobalSLALatencyNS(50_000_000);
 		
 		
-		JSONExtractorCompleted extractor =
-				builder.defineJSONExtractor()
-				.newPath(JSONType.TypeString).key("key1").completePath("name_a", Fields.nameA)
-		        .newPath(JSONType.TypeInteger).key("key2").completePath("name_b", Fields.nameB);
+		JSONDecoder extractor =
+				builder.defineJSONSDecoder().begin()
+					.element(JSONType.TypeString).key("key1").asField(Fields.nameA)
+		        	.element(JSONType.TypeInteger).key("key2").asField(Fields.nameB)
+				.finish();
 		
 		
 		int aRouteId = builder.defineRoute(extractor).path("/te${value}")
 				        .associatedObject("value", Fields.urlArg)
 				        .routeId();
 		
-		fieldA  = builder.lookupFieldByName(aRouteId,"name_a");
-		fieldB  = builder.lookupFieldByName(aRouteId,"name_b");
+		fieldA  = builder.lookupFieldByName(aRouteId,"nameA");
+		fieldB  = builder.lookupFieldByName(aRouteId,"nameB");
+
 		long fieldB2 = builder.lookupFieldByIdentity(aRouteId, Fields.nameB);
 		assert(fieldB==fieldB2);
 		long fieldL  = builder.lookupFieldByIdentity(aRouteId, HTTPHeaderDefaults.CONTENT_LENGTH);
