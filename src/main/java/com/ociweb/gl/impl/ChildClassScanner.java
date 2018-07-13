285328d7f14b1ee87bee7cc9e69c13e7a0c207c6
nate <nate@Enoch>
2018-07-12 23:28:43 -0500

    faster scan logic up tree of classes
    removed dynamic functions from FixedTopicSerivce

--- src/main/java/com/ociweb/gl/impl/ChildClassScanner.java
+++ src/main/java/com/ociweb/gl/impl/ChildClassScanner.java
@@ -4,6 +4,8 @@ import com.ociweb.gl.api.MsgRuntime;
 import com.ociweb.pronghorn.pipe.ChannelWriter;
 import com.ociweb.pronghorn.pipe.util.hash.IntHashTable;
 import com.ociweb.pronghorn.util.Appendables;
+import com.ociweb.pronghorn.util.TrieParser;
+import com.ociweb.pronghorn.util.TrieParserReaderLocal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +15,7 @@ import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * Scans up the object tree to find instances of specific classes
@@ -47,6 +50,9 @@ public class ChildClassScanner {
 	///////////
 	///////////
 
+	static TrieParser textToBlock  = buildBlockingTypes();
+	
+	
 	static boolean visitByClass(String topName, Object listener, int depth, 
 												 ChildClassScannerVisitor visitor,
 												 Class<? extends Object> c,
@@ -82,33 +88,9 @@ public class ChildClassScanner {
 								    && (!Externalizable.class.isAssignableFrom(cType))								    
 								    
 								    && (!(name=cType.getName()).startsWith("java.")) 
-									&& (!name.startsWith("[Ljava."))
-									&& (!name.startsWith("[["))
-									&& (!name.startsWith("[I"))
-									&& (!name.startsWith("[B"))
-									&& (!name.startsWith("[J"))
-									&& (!name.startsWith("[S"))
-									
-									&& (!name.startsWith("com.ociweb.pronghorn.stage."))
-									&& (!name.startsWith("[Lcom.ociweb.pronghorn.stage."))
-									
-									&& (!name.startsWith("com.ociweb.iot.hardware."))
-									&& (!name.startsWith("[Lcom.ociweb.iot.hardware."))
-																	
-									&& (!name.startsWith("com.ociweb.pronghorn.pipe."))  
-									&& (!name.startsWith("[Lcom.ociweb.pronghorn.pipe.")) 
-									
-									&& (!name.startsWith("com.ociweb.pronghorn.util."))  
-									&& (!name.startsWith("[Lcom.ociweb.pronghorn.util.")) 
-									
-									&& (!name.startsWith("com.ociweb.pronghorn.network."))  
-									&& (!name.startsWith("[Lcom.ociweb.pronghorn.network.")) 
-									
-									&& (!name.startsWith("com.ociweb.gl.impl."))  
-									&& (!name.startsWith("[Lcom.ociweb.gl.impl."))											
-									
-									&& (!name.startsWith("org.slf4j."))
-																				
+								    
+								    && (-1 == TrieParserReaderLocal.get().query(textToBlock, name))
+								    																				
 										) {
 										
 									
@@ -133,32 +115,9 @@ public class ChildClassScanner {
 									&&  (!obj.getClass().isPrimitive()) 
 									&& (obj != listener) 
 									&& (!(name=obj.getClass().getName()).startsWith("java.")) 
-									&& (!name.startsWith("[Ljava."))
-									&& (!name.startsWith("[["))
-									&& (!name.startsWith("[I"))
-									&& (!name.startsWith("[B"))
-									&& (!name.startsWith("[J"))
-									&& (!name.startsWith("[S"))
 									
-									&& (!name.startsWith("com.ociweb.pronghorn.stage."))
-									&& (!name.startsWith("[Lcom.ociweb.pronghorn.stage."))
-									
-									&& (!name.startsWith("com.ociweb.iot.hardware."))
-									&& (!name.startsWith("[Lcom.ociweb.iot.hardware."))
-																	
-									&& (!name.startsWith("com.ociweb.pronghorn.pipe."))  
-									&& (!name.startsWith("[Lcom.ociweb.pronghorn.pipe.")) 
-									
-									&& (!name.startsWith("com.ociweb.pronghorn.util."))  
-									&& (!name.startsWith("[Lcom.ociweb.pronghorn.util.")) 
-									
-									&& (!name.startsWith("com.ociweb.pronghorn.network."))  
-									&& (!name.startsWith("[Lcom.ociweb.pronghorn.network.")) 
+									&& (-1 == TrieParserReaderLocal.get().query(textToBlock, name))
 																		
-									&& (!name.startsWith("com.ociweb.gl.impl."))  
-									&& (!name.startsWith("[Lcom.ociweb.gl.impl."))
-							
-									&& (!name.startsWith("org.slf4j."))
 									&& (!obj.getClass().isEnum())
 									&& (!(obj instanceof MsgRuntime))  
 									&& (!(obj instanceof ChannelWriter))  
@@ -172,6 +131,8 @@ public class ChildClassScanner {
 								
 								if (!seen.contains(obj)) {
 									seen.add(obj);
+									
+									//System.out.println("     "+depth+"  "+obj.getClass().getName());
 								                		//if (depth >4 ) {
 								                		//	logger.info(depth+" "+obj.getClass().getName());
 								                		//}
@@ -193,12 +154,45 @@ public class ChildClassScanner {
 	        return true;
 		}
 
+	private static TrieParser buildBlockingTypes() {
+		TrieParser map = new TrieParser(400, 1, false, false, false);
+		
+		map.setUTF8Value("[Ljava.",1);
+        map.setUTF8Value("[[",1);
+		map.setUTF8Value("[I",1);
+		map.setUTF8Value("[B",1);
+		map.setUTF8Value("[J",1);
+		map.setUTF8Value("[S",1);
+				
+		map.setUTF8Value("com.ociweb.pronghorn.stage.",1);
+		map.setUTF8Value("[Lcom.ociweb.pronghorn.stage.",1);
+				
+		map.setUTF8Value("com.ociweb.iot.hardware.",1);
+		map.setUTF8Value("[Lcom.ociweb.iot.hardware.",1);
+												
+		map.setUTF8Value("com.ociweb.pronghorn.pipe.",1); 
+		map.setUTF8Value("[Lcom.ociweb.pronghorn.pipe.",1);
+				
+		map.setUTF8Value("com.ociweb.pronghorn.util.",1); 
+		map.setUTF8Value("[Lcom.ociweb.pronghorn.util.",1);
+				
+		map.setUTF8Value("com.ociweb.pronghorn.network.",1);  
+		map.setUTF8Value("[Lcom.ociweb.pronghorn.network.",1);
+													
+		map.setUTF8Value("com.ociweb.gl.impl.",1); 
+		map.setUTF8Value("[Lcom.ociweb.gl.impl.",1);
+		
+		map.setUTF8Value("org.slf4j.",1);
+		
+		return map;
+	}
+
 	static boolean visitUsedByClass(String name, Object obj, int depth, ChildClassScannerVisitor visitor, 
 			     Object topParent, Class targetType, Collection<Object> seen) {
 		
 	    if (null!=obj) {
 		    Class<? extends Object> c = obj.getClass();
-		    while (null != c) {
+		    while ((null!=c) && (c!=Object.class)) {
 		    	if (!visitByClass(name, obj, depth, visitor, c, targetType, topParent, seen)) {
 		    		return false;
 		    	}
@@ -210,11 +204,11 @@ public class ChildClassScanner {
 
 	public static boolean visitUsedByClass(String name, Object listener, ChildClassScannerVisitor visitor, Class target) {
 		long start = System.nanoTime();
-		boolean result = visitUsedByClass(name, listener, 0, visitor, listener, target, new ArrayList<Object>());
+		boolean result = visitUsedByClass(name, listener, 0, visitor, listener, target, new HashSet<Object>());
 		long duration = System.nanoTime()-start;
 		
 		//just report the longest duration
-		if (duration > 1_000_000) {
+		if (duration > 10_000_000) {
 			Appendables.appendNearestTimeUnit(System.out, duration);
 			System.out.println(" duration for scan to find all "+target.getSimpleName()+" instances inside "+listener.getClass().getSimpleName());
 		}
