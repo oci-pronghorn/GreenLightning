bc0bafb77602dd08b7c15720449b2598169e1ab9
nate <nate@Enoch>
2018-08-06 20:24:31 -0500

    clean up imports

--- src/main/resources/archetype-resources/src/test/java/AppTest.java
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -1,12 +1,8 @@
 package ${package};
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 import com.ociweb.gl.api.GreenRuntime;
-import com.ociweb.pronghorn.stage.scheduling.ScriptedNonThreadScheduler;
 
 /**
  * Unit test for simple App.
